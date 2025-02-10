5eb0ef569efd1b08257a39a47d7f67d31f127035
prakash <apuiirom@gmailcom>
2025-02-10 19:45:47 +0530

    Top Bar Added

--- app/src/main/java/com/apui/androiddevicemonitor/MainActivity.kt
+++ app/src/main/java/com/apui/androiddevicemonitor/MainActivity.kt
@@ -1,31 +1,41 @@
 package com.apui.androiddevicemonitor
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.apui.androiddevicemonitor.ui.theme.AndroidDeviceMonitorTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val splashScreen = installSplashScreen()
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             AndroidDeviceMonitorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopBar()
+                    }
+                ) {
+
                 }
             }
         }
@@ -33,17 +43,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TopBar() {
+    Row(
+        modifier = Modifier
+            .padding(top = 30.dp)
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.icons8health75),
+            contentDescription = "Android Device Monitor Icon"
+        )
+        Spacer(modifier = Modifier.width(5.dp))
+        Text("Android Device Monitor")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ShowPreview() {
     AndroidDeviceMonitorTheme {
-        Greeting("Android")
+        TopBar()
     }
 }
